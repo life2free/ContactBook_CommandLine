b8f84ad38857640d11a5eaaccbe44688401b019a
shimin <thunderous_0518@hotmail.com>
2020-04-30 13:28:04 -0400

    finish the basic feature of get all contacts

--- src/main.py
+++ src/main.py
@@ -37,7 +37,7 @@ hattie = Contact(first_name='Hattie', last_name='Mora',
                  phone='7680735403', company='Facebook')
 hattie.save()
 
-roger = Contact(first_name='Roger', last_name='Campbel',
+roger = Contact(first_name='Roger', last_name='Campbell',
                 phone='7000302403', company='General Assembly')
 roger.save()
 
@@ -48,3 +48,82 @@ noah.save()
 shimin = Contact(first_name='Shimin', last_name='Rao',
                  phone='7623735403', company='General Assembly')
 shimin.save()
+
+
+class Main:
+    def __init__(self):
+        self.title = "Welcome to Contact Book!"
+
+    def start(self):
+        print(f"\n{self.title}\n")
+        self.run()
+
+    def run(self):
+        print("Please choice the options as below:")
+        print(" 1. Get all contacts.")
+        print(" 2. Get a single contact.")
+        print(" 3. Create a new contact.")
+        print(" 4. Update a contact.")
+        print(" 5. Delete a contact.")
+        print(" 6. Exit the app.")
+        print()
+        try:
+            choice = int(input("Please type the option number: "))
+            if(choice > 6):
+                self.try_again_with_invalid_input(self.run)
+            else:
+                if(choice == 1):
+                    print("1")
+                    self.get_all_contacts()
+                if(choice == 2):
+                    print("2")
+                if(choice == 3):
+                    print("3")
+                if(choice == 4):
+                    print("4")
+                if(choice == 5):
+                    print("5")
+                if(choice == 6):
+                    print("Exit the app!")
+        except ValueError as e:
+            self.try_again_with_invalid_input(self.run)
+
+    def get_all_contacts(self):
+        print("Please choice the options as below:")
+        print(" 1. Only list the firstname and lastname.")
+        print(" 2. List all of the information.")
+        print(" 3. Back to main options.")
+        print(" 4. Exit the app.")
+        try:
+            choice = int(input("Please type the option number: "))
+            if(choice > 4):
+                self.try_again_with_invalid_input(self.get_all_contacts)
+            else:
+                all_contacts = Contact.select()
+                if(choice == 1):
+                    for contact in all_contacts:
+                        print(f"    {contact.first_name} {contact.last_name}")
+                    self.back_to_main()
+                if(choice == 2):
+                    print("2")
+                if(choice == 3):
+                    print("3")
+                    self.back_to_main()
+                if(choice == 4):
+                    print("Exit the app!")
+        except ValueError as e:
+            self.try_again_with_invalid_input(self.get_all_contacts)
+
+    def try_again_with_invalid_input(self, func):
+        input('Invalid input, Press Enter try again.')
+        print()
+        func()
+
+    def back_to_main(self):
+        input("Press Enter to back to main options.")
+        print()
+        self.run()
+
+
+contact_book = Main()
+contact_book.start()
